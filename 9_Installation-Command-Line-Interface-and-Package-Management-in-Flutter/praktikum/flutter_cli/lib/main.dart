48faf405e961339b1633f61d7ace08ff2607889d
ferya2 <adefery221@gmail.com>
2023-09-04 23:10:13 +0700

    modifikasi untuk soal prioritas 2

--- 9_Installation-Command-Line-Interface-and-Package-Management-in-Flutter/praktikum/flutter_cli/lib/main.dart
+++ 9_Installation-Command-Line-Interface-and-Package-Management-in-Flutter/praktikum/flutter_cli/lib/main.dart
@@ -1,34 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/percent_indicator.dart';
+// import 'package:percent_indicator/percent_indicator.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: CircularPercentIndicator(
+//           radius: 60.0,
+//           lineWidth: 5.0,
+//           percent: 1.0,
+//           center: const Text("100%"),
+//           progressColor: Colors.red,
+//         ),
+//       ),
+//     );
+//   }
+// }
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: CircularPercentIndicator(
-          radius: 60.0,
-          lineWidth: 5.0,
-          percent: 1.0,
-          center: const Text("100%"),
-          progressColor: Colors.red,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Halo Alterra"), 
+          backgroundColor: const Color.fromARGB(255, 21, 138, 25),
+        ),
+        body: const Center(
+          child: Text("Nama saya adalah Ade Fery Angriawan"), 
         ),
       ),
     );
